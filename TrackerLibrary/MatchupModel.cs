977ffe8be970f7bb551e571158138a5ae1a4fce2
jatwell93 <atwellj93@gmail.com>
2021-09-26 14:39:37 +1000

    add models and xml

--- TrackerLibrary/MatchupModel.cs
+++ TrackerLibrary/MatchupModel.cs
@@ -6,9 +6,24 @@ using System.Threading.Tasks;
 
 namespace TrackerLibrary
 {
+    /// <summary>
+    /// Represents one match in the tournament 
+    /// </summary>
     public class MatchupModel
     {
+        /// <summary>
+        /// The set of teams that were involved in this matchup
+        /// </summary>
         public List<MatchupEntryModel> Entries { get; set; }
-        
+
+        /// <summary>
+        /// The winner of the mactch
+        /// </summary>
+        public TeamModel MatchupWinner { get; set; }
+
+        /// <summary>
+        /// Which round this match is a part of 
+        /// </summary>
+        public int MatchupRound { get; set; }
     }
 }
--- TrackerLibrary/PersonModel.cs
+++ TrackerLibrary/PersonModel.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace TrackerLibrary
 {
+    /// <summary>
+    /// Represents one person
+    /// </summary>
     public class PersonModel
     {
+        /// <summary>
+        /// Represents the first name of the person 
+        /// </summary>
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// Represents the surname of the person
+        /// </summary>
         public string LastName { get; set; }
+
+        /// <summary>
+        /// Represents the email of the person 
+        /// </summary>
         public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Represents the mobile of the person 
+        /// </summary>
         public string MobileNumber { get; set; }
     }
 }
